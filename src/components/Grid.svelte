527cbfe368fd67ab7d03b88f67f36babd6d5ba70
Benjamin Dryden <benjamin@adventureclub.io>
2020-12-02 13:36:43 +0100

    Grid outlines

---
+++ src/components/Grid.svelte
@@ -0,0 +1,69 @@
+<script>
+
+</script>
+
+<section>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+</section>
+
+<style lang="scss">
+  @import './src/scss/global';
+
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    gap: 0px 0px;
+    grid-template-areas:
+    ". . ."
+    ". . ."
+    ". . ."
+    ". . ."
+    ". . ."
+    ". . ."
+    ". . ."
+    ". . ."
+    ". . ."
+    ". . .";
+
+    .grid-item {
+      height: 10rem;
+      background: cornflowerblue;
+      @include respond-to('desktop') {
+        background: hotpink;
+
+      }
+
+    }
+
+    @include respond-to('desktop') {
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr; //6 for desktop
+        grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr; //10 for now
+        grid-template-areas:
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . ."
+    ". . . . . .";
+    }
+  }
+
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,17 @@
-
+<script>
+	import Grid from '../components/Grid.svelte'
+</script>
 <svelte:head>
 	<title>Merry Xmas Ya Filthy Animal</title>
 	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
 	<link rel="icon" href="./favicon.ico" type="image/x-icon">
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<figcaption>Happy Boi Fun Times</figcaption>
-</figure>
-
-<p><strong>New AC Project Template</strong></p>
+<Grid />
 
 <style type="scss">
 		//import globals to access globals, mixins etc
 		@import './src/scss/global';
 
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-		font-family: 'Gilroy Regular', Arial, SansSerif;
-		@include respond-to('desktop') {
-			font-family: 'Gilroy Semibold', Arial, SansSerif;
-		}
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-		figcaption {
-			background: hotpink;
-		}
-	}
-
-	img {
-		width: 100%;
-		max-width: 200px;
-		margin: 0 0 1em 0;
-	}
 
 </style>
